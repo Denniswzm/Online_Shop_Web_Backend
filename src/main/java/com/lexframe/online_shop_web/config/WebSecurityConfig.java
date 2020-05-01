f6cf5e74ad39b9b9811d48a24b077cade010c7c7
Denniswzm <denniswzm@gmail.com>
2020-04-30 22:25:54 -0400

    Updating User & Password Authentication

--- src/main/java/com/lexframe/online_shop_web/config/WebSecurityConfig.java
+++ src/main/java/com/lexframe/online_shop_web/config/WebSecurityConfig.java
@@ -1,25 +1,45 @@
 package com.lexframe.online_shop_web.config;
 
+import com.lexframe.online_shop_web.repository.UserRepository;
+import com.lexframe.online_shop_web.service.DBUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //...
+//        super.configure(auth);
+        auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/").permitAll()
+            .anyRequest().authenticated().and()
+                .formLogin()
+            .loginProcessingUrl("/login");
     }
 
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new DBUserDetailService(userRepository);
+    }
 
 }
---
+++ src/main/java/com/lexframe/online_shop_web/service/DBUserDetailService.java
@@ -0,0 +1,74 @@
+package com.lexframe.online_shop_web.service;
+
+import com.lexframe.online_shop_web.model.User;
+import com.lexframe.online_shop_web.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class DBUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public DBUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new DBUserDetails(user.get());
+    }
+}
+
+class DBUserDetails implements UserDetails {
+    private final User user;
+
+    public DBUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
